6ab25febb9c8e22f148775278a6edd14ac8db684
Jack5241027 <jack5241027@gmail.com>
2018-08-24 02:55:19 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/commons/manifest.js
+++ out/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "0028af782cbef5485c2b"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "69a5ea4fe5207f035572"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
